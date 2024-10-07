b05593538fcef7dba7d9a1ad3364560e6bcb594d
elamribadrayour <badrayour.elamri@protonmail.com>
2024-10-07 09:37:04 +0200

    [FEATURE] scikit-tsfel - lint & publish

--- setup.py
+++ setup.py
@@ -2,24 +2,22 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="tsfel-scikit",
     version="0.1.0",
+    name="tsfel-scikit",
+    packages=find_packages(),
     author="Badr Ayour EL AMRI",
     author_email="badrayour.elamri@protonmail.com",
-    description="A Python package for TSFEL feature extraction with scikit-learn integration",
-    long_description=open("readme.md").read(),
-    long_description_content_type="text/markdown",
     url="https://github.com/elamribadrayour/scikit-tsfel",
-    packages=find_packages(),
+    description="A Python package for TSFEL feature extraction with scikit-learn integration",
     install_requires=[
         "tsfel",
         "pandas",
         "scikit-learn",
     ],
     classifiers=[
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
     python_requires=">=3.11",
 )
